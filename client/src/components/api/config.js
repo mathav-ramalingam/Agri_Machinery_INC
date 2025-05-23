cc77646d8e85a28e925214016771548cab0b3fbe
mathav-ramalingam <mathavramalingam1608@gmail.com>
2025-05-23 09:59:54 +0530

    Backend Hosted

--- client/src/components/api/config.js
+++ client/src/components/api/config.js
@@ -1,2 +1,4 @@
-export const BASE_URL = "http://localhost:5000";
-// export const BASE_URL = "https://agri-machinery-inc.onrender.com";
+// export const BASE_URL = "http://localhost:5000";
+export const BASE_URL = "https://agri-machinery-inc.onrender.com";
+
+// https://agri-machinery-inc.onrender.com
